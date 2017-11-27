79c4bc6c0b9467014f2215d636478d844100852a
Siarhei Mileuski <siarhei.mileuski@mobica.com>
2017-11-27 20:58:50 +0100

    rxSearch

--- app/src/main/java/com/sergey/stackoverflowtest/search/MainActivity.java
+++ app/src/main/java/com/sergey/stackoverflowtest/search/MainActivity.java
@@ -1,18 +1,64 @@
 package com.sergey.stackoverflowtest.search;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.jakewharton.rxbinding2.widget.RxTextView;
 import com.sergey.stackoverflowtest.MyApplication;
 import com.sergey.stackoverflowtest.R;
 import com.sergey.stackoverflowtest.search.presenter.MainActivityPresenter;
 
-public class MainActivity extends AppCompatActivity implements MainActivityPresenter.MainActivityUI{
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+
+
+public class MainActivity extends AppCompatActivity implements MainActivityPresenter.MainActivityUI {
+
+    @Inject
+    MainActivityPresenter presenter;
+    private EditText editText;
+    private Disposable disposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         ((MyApplication) getApplication()).getAppComponent().inject(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.search_edit_text);
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        presenter.attacheUI(this);
+        disposable = RxTextView.textChanges(editText)
+                .debounce(1L, TimeUnit.SECONDS)
+                .filter(charSequence -> charSequence.length() > 3)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(sequence -> presenter.startSearch(sequence));
+
+    }
+
+    @Override
+    public void showResultOfSearch(CharSequence charSequence) {
+        Toast.makeText(this, charSequence, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        presenter.detachUI();
+        if (disposable != null) {
+            disposable.dispose();
+        }
+        super.onPause();
+
     }
 }
--- app/src/main/java/com/sergey/stackoverflowtest/search/presenter/MainActivityPresenter.java
+++ app/src/main/java/com/sergey/stackoverflowtest/search/presenter/MainActivityPresenter.java
@@ -3,12 +3,17 @@ package com.sergey.stackoverflowtest.search.presenter;
 import com.sergey.stackoverflowtest.BasePresenter;
 import com.sergey.stackoverflowtest.UI;
 
+import javax.inject.Inject;
+
 /**
  * Created by sergey on 27.11.17.
  */
 
 public class MainActivityPresenter extends BasePresenter<MainActivityPresenter.MainActivityUI> {
 
+    @Inject
+    public MainActivityPresenter() {
+    }
 
     @Override
     public void attacheUI(MainActivityUI ui) {
@@ -20,7 +25,14 @@ public class MainActivityPresenter extends BasePresenter<MainActivityPresenter.M
         super.detachUI();
     }
 
+
+    public void startSearch(CharSequence charSequence){
+     ui.showResultOfSearch(charSequence);
+    }
+
     public interface MainActivityUI extends UI {
 
+
+        void showResultOfSearch(CharSequence charSequence);
     }
 }
